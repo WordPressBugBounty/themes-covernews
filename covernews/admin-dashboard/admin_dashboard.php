11e66febbc8d911d05a1ce20c0456096a7aff2c1
Sean Murphy <sean@iamseanmurphy.com>
2026-07-27 15:14:19 +0000

    7.4.4

--- covernews/admin-dashboard/admin_dashboard.php
+++ covernews/admin-dashboard/admin_dashboard.php
@@ -406,7 +406,7 @@ if (!class_exists('AF_themes_info')) {
       wp_enqueue_script(
         'aftheme-dashboard', // Handle.
         get_template_directory_uri() . '/admin-dashboard/dist/admin_dashboard.build.js',
-        array('react', 'react-dom', 'wp-api-fetch', 'wp-element'), // Dependencies, defined above.
+        array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), // Added 'wp-i18n'
         '1.0.0',
         true
       );
@@ -466,6 +466,26 @@ if (!class_exists('AF_themes_info')) {
         'activate_btn' => __('Activate', 'covernews'),
         'installed_btn' => __('Activated', 'covernews')
       ));
+      // 1. Core Dashboard React App
+      wp_set_script_translations(
+        'aftheme-dashboard',
+        'covernews',
+        get_template_directory() . '/languages' // <-- Tells WP where to find source hashes
+      );
+
+      // 2. Plugin Installer
+      wp_set_script_translations(
+        'plugin-installer',
+        'covernews',
+        get_template_directory() . '/languages'
+      );
+
+      // 3. TemplateSpare Installer
+      wp_set_script_translations(
+        'templatespare-installer',
+        'covernews',
+        get_template_directory() . '/languages'
+      );
     }
 
     function covernews_get_latest_changelog()
